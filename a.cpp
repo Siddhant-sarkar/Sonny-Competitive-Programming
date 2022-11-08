2ac6af9bae6c1fe891cc4051420103a99742aca5
Siddhant Sarkar <sidzensarkar@Siddhants-MacBook-Air.local>
2022-11-09 04:47:05 +0530

    Initial COmmit

---
+++ a.cpp
@@ -0,0 +1,24 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define nl "\n"
+#define int long long
+
+#define pb push_back
+#define all(s) s.begin(), s.end()
+#define BANKAI cin.tie(0)->sync_with_stdio(0);
+
+#ifndef ONLINE_JUDGE
+#include "bits/bug.hpp"
+#else
+#define dbg(...)
+#endif
+
+constexpr int MOD = 1e9 + 7;
+constexpr int INF  = 1e18;
+const int N = 2e5 + 10;
+
+int n , e;
+int32_t main() {
+	cin >> n >> e;
+	bug(n, e);
+}
---
+++ lib/dsu.cpp
@@ -0,0 +1,26 @@
+#include"bits/stdc++.h"
+using namespace std;
+
+struct DSU {
+	vector<int> par, sz;
+	DSU(int n) {
+		par = vector<int> (n + 10); sz = vector<int> (n + 10);
+		for (int i = 0; i <= n; i++) par[i] = i, sz[i] = 1;
+	}
+	int get(int i) {
+		if (par[i] == i) return i;
+		return par[i] = get(par[i]);
+	}
+	bool unn(int a, int b) {
+		a = get(a), b = get(b);
+		if ( a == b) return 0;
+		if (sz[a] < sz[b]) swap(a, b);
+		par[b] = a; sz[a] += sz[b];
+		return 1;
+	}
+	int sze(int a) { return sz[get(a)];}
+};
+
+int main(){
+
+}
