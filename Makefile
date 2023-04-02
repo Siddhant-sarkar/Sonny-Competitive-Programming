f972f3e278033f98ef79faef391240819f6a5745
ssarkar-be20 <ssarkar_be20@thapar.edu>
2023-04-02 23:22:46 +0530

    This was good af

--- Makefile
+++ Makefile
@@ -1,15 +1,15 @@
 CC = g++-12
 CFLAGS = -std=c++20 -O2 -Wall -Wshadow -Wno-unused-variable -Wfloat-equal -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=undefined -fno-sanitize-recover -DTSUKI 
+CFFLAGS = -std=c++20 -O2 -Wall -Wshadow -Wno-unused-variable -Wfloat-equal -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=undefined -fno-sanitize-recover  
 TARGET = _a       
 SRC = main.cpp       
 
 .PHONY: all 
 all: $(TARGET)
 	@gtimeout 2s ./$(TARGET) < inputf.in && echo 'Press any key to continue...' && read line && clear 
+.PHONY: inp
 inp: $(TARGET)
 	@echo 'Enter now ...' && ./$(TARGET) && echo 'Press any key to continue...' && read line && clear 
-
-
 $(TARGET):	$(SRC)
 	@$(CC) $(CFLAGS) $(SRC) -o $(TARGET)
 clean:
--- main.cpp
+++ main.cpp
@@ -2,23 +2,22 @@
 using namespace std;
 
 // TYPE NAME ALIASEs
-#define int 		long long
-using vi 	= 		vector<int>;
-using vv 	= 		vector<vi>;
-using pi 	= 		pair<int, int>;
-using vp 	= 		vector<pi>;
+#define int 			long long
+using vi 		= 		vector<int>;
+using vv 		= 		vector<vi>;
+using pi 		= 		pair<int, int>;
+using vp 		= 		vector<pi>;
 
 // DEFINE MACROS
-#define nl 			"\n"
-#define F 			first
-#define S 			second
-#define pb 			push_back
-#define sz(x) 		(int)x.size()
-#define read(x) 	int x; cin >>x;
-#define all(s) 		s.begin(), s.end()
-#define rep(i,a,b) 	for(int i=a;i<=b;i++)
-#define rev(i,a,b) 	for(int i=a;i>=b;i--)
-#define rdvr(a,n)   vi a(n);for(auto &it :a) cin >> it
+#define nl 				"\n"
+#define F 				first
+#define S 				second
+#define pb 				push_back
+#define sz(x) 			(int)x.size()
+#define read(x) 		int x; cin >>x;
+#define all(s) 			s.begin(), s.end()
+#define loop(i,a,b) 	for(int i=a;i<=b;i++)
+#define roop(i,a,b) 	for(int i=a;i>=b;i--)
 
 // CUSTOM HASH
 struct chash {
@@ -28,6 +27,12 @@ struct chash {
 		return x ^ (x >> 16);
 	}
 };
+int fx_ceil(int a, int b) { // ceil(a / b )
+	return (a / b) + (a % b != 0);
+}
+int fx_lcm(int a, int b) {
+	return a * b / gcd(a, b);
+}
 
 //DEBUG HEADER FILE
 #ifdef TSUKI
@@ -38,25 +43,22 @@ struct chash {
 
 //CONSTANTS
 constexpr int MOD = 998244353;
-constexpr int N = 2e5;
-//-----------------------------------私はマスターです -----------------------------------//
+constexpr int MXN = 2e5;
+constexpr int MX = 1e18;
+
 void サンローラン_ドン() {
-	read(n); read(k);
-	string a , b, x, y;
-	cin >> a >> b;
-	x = a, y = b; sort(all(x)); sort(all(y));
-	if (x != y ) { cout << "NO" << nl; return; }
-
+	int n; cin >> n;
+	cout << n << nl;
+	cout << "this is not coloured in c++ string " << nl;
 }
-
 int32_t main() {
 	clock_t tStart = clock();
 	cin.tie(0); cout.tie(0) -> sync_with_stdio(0);
 	int t = 1;
 	cin >> t;
-	rep(i, 1, t) {
-		// cerr << "==>\tTest " << i << nl;
+	loop(i, 1, t) {
+		cerr << "\033[1;32m==>\tTest " << i << "\033[1;0m" << nl;
 		サンローラン_ドン();
 	}
-	cerr << "\n>> Runtime : " << (double)(clock() - tStart) / CLOCKS_PER_SEC << nl;
+	cerr << "\033[1;34m\n>> Runtime : " << (double)(clock() - tStart) / CLOCKS_PER_SEC << nl;
 }
