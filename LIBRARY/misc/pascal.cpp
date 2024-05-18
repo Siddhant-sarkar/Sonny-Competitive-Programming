3db9b897e18e56228df3cdf3a3a58b307fc60c1c
Sonny Sarkar <sonny@Sonnys-MacBook-Air.local>
2024-05-18 10:29:36 +0530

    this was so great

--- LIBRARY/misc/pascal.cpp
+++ LIBRARY/misc/pascal.cpp
@@ -10,29 +10,20 @@ constexpr ll INF = 1e18;
 
 // pascal triangle
 vector<vector<ll>> nCR;
-void pre() {
+void pre(){
 	ll k = 1e3;
-	nCR =  vector<vector<ll>>(k + 10, vector<ll> (k + 10));
+	nCR =  vector<vector<ll>>(k+10, vector<ll> (k+10));
 	nCR[0][0] = 1LL;
-	FOR(n, 1, k) {
+	FOR(n,1,k){
 		nCR[n][0] = nCR[n][n] = 1LL;
-		FOR(r, 1, k) {
-			(nCR[n][r] = nCR[n - 1][r] + nCR[n - 1][r - 1]) %= MOD;
+		FOR(r, 1, k){
+			(nCR[n][r] = nCR[n-1][r] + nCR[n-1][r-1]) %= MOD;
 		}
 	}
 }
 
-void cp() {
-	ll N ; cin >> N;
-	vector<ll> A(N);
-	FOR(i, 0, N - 1) cin >> A[i];
+void cp(){
 
-	ll res = 1LL, total = 0;
-	FOR(i, 0, N - 1) {
-		(res *= (nCR[total + A[i] - 1][A[i] - 1])) %= MOD;
-		total += A[i];
-	}
-	cout << res << "\n";
 }
 
 int32_t main() {
