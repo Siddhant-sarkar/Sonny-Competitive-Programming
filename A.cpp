acd45b955df27fd890d3b1188bd5f5b849f36fc5
Sonny Sarkar <sonny@Sonnys-MacBook-Air.local>
2024-07-27 20:06:21 +0530

    this is finally implmented

--- A.cpp
+++ A.cpp
@@ -1,16 +1,38 @@
 #include"bits/stdc++.h"
 using namespace std;
-
-#define all(x) begin(x), end(x)
 #define FOR(i,a,b) for(int i=a;i<=b;i++)
-const int MX = 1e5+10;	
- 
-struct query{
-	char a;
-	ll l , r; 
-} q[MXN];
-ll n, tree[MXN];
-int main(){
-	cin.tie(0); cout.tie(0) -> sync_with_stdio(0);
-	
-} 
+
+int main()
+{
+	int n , e; cin >> n >> e;
+	vector<vector<int>> graph(n + 1);
+	FOR(i, 1, n) {
+		int u, v ; cin >> u >> v;
+		graph[u].push_back(v);
+		graph[v].push_back(u);
+	}
+	vector<int> vis(n + 1), p(n + 1);
+	queue<int> q; q.push(1);
+	while (!q.empty()) {
+		int u = q.front(); q.pop();
+		vis[u] = 1;
+		if ( u == n) {
+			vector<int> a;
+			while (u) {
+				a.push_back(u);
+				u = p[u];
+			}
+			for (auto x : a )
+				cout << x << " ";
+			cout << endl;
+
+		}
+		for (auto v : graph[u] ) {
+			if (!vis[v]) {
+				q.push(v);
+				p[v] = u;
+			}
+		}
+	}
+}
+
---
+++ LIBRARY/graph/bipartitie.cpp
@@ -0,0 +1,36 @@
+#include"bits/stdc++.h"
+using namespace std;
+#define FOR(i,a,b) for(int i=a;i<=b;i++)
+
+int main()
+{
+	cin.tie(0); cout.tie(0) -> sync_with_stdio(0);
+	int N , E ; cin >> N >> E;
+	vector<vector<int>> graph(N + 1);
+	FOR(i, 1, E) {
+		int u, v ; cin >> u >> v;
+		graph[u].push_back(v);
+		graph[v].push_back(u);
+	}
+	vector<int> vis(N + 1, -1);
+	auto dfs  = [&] (auto && dfs , int u , int p, int c )->bool{
+		if ( vis[u] != -1) return vis[u] == c ? 1 : 0;
+		vis[u] = c;
+		bool ans = 1;
+		for (auto v : graph[u])
+		{
+			ans &= dfs(dfs , v , u, 1 - c);
+		}
+		return ans;
+	};
+	FOR(i, 1, N) {
+		if ( vis[i] == -1) {
+			if ( !dfs(dfs, i, 0, 1)) {
+				// the graph cannot be bipartiite
+				break;
+			}
+		}
+	}
+	// the graph is bibartitie
+}
+
